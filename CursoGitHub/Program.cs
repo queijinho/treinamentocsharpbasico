6a73f46b7de590329fd1935c951e4408c5bf7e7a
Nutelinha <matheus.rlima9@aluno.senai.br>
2022-07-04 11:31:57 -0300

    first commit

---
+++ CursoGitHub/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CursoGitHub.InterfaceConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            helloworld();
+        }
+
+        private static void helloworld()
+        {
+            Console.WriteLine("Hello world!!");
+            Console.ReadKey();
+        }
+    }
+}
